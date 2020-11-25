8d494e09379167a6e6df3179fa0f68aeb79dafca
CJ Celimen <cjdcelimen@gmail.com>
2020-11-25 22:20:09 +0800

    Update jobs with Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import useFetchJobs from './useFetchJobs';
 import { Container } from 'react-bootstrap'
+import Job from './Job'
 
 function App() {
   const [params, setParams] = useState({})
@@ -8,10 +9,12 @@ function App() {
   const { jobs, loading, error } = useFetchJobs(params, page)
   
   return (
-    <Container>
+    <Container className='my-4'>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error...</h1>}
-      {<h1>{jobs.length}</h1>}
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
     </Container>
   );
 }
